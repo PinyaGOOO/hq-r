9427e9a2ad5864ee9ad467ca7e68404df33c7171
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-08 20:52:34 +0300

    Add files via upload

--- hq-r.sh
+++ hq-r.sh
@@ -22,7 +22,7 @@ systemctl enable --now nftables
 nmcli con add type ip-tunnel ifname tun1 mode gre remote 2.2.2.2 local 1.1.1.2
 nmcli con modify ip-tunnel-tun1 ipv4.method manual ipv4.addresses 10.10.10.1/30
 nmcli con modify ip-tunnel-tun1 ipv6.method manual ipv6.addresses FD24:10::1/64
-nmcli conniction modify tun1 ip-tunnel.ttl 64
+nmcli connection modify tun1 ip-tunnel.ttl 64
 sed -i '11i\parent=ens18' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 sed -i '/id=ip-tunnel-tun1/d' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 sed -i '2i\id=tun1' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
