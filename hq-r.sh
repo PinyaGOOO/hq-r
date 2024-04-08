41456a7165a729fbdcb5bfe68cdc2469353cd6ac
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-08 18:12:05 +0300

    Add files via upload

---
+++ hq-r.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses 2024:1::2/64
+nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.gateway 2024:1::1
+nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 1.1.1.2/30
+nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.gateway 1.1.1.1
+
+nmcli con modify Проводное\ подключение\ 2 ipv6.method manual ipv6.addresses FD24:172::1/122
+nmcli con modify Проводное\ подключение\ 2 ipv4.method manual ipv4.addresses 172.16.100.1/26
+
+nmcli con modify Проводное\ подключение\ 3 ipv6.method manual ipv6.addresses 2024:4::1/64
+nmcli con modify Проводное\ подключение\ 3 ipv6.method manual ipv6.gateway 2024:4::2/64
+nmcli con modify Проводное\ подключение\ 3 ipv4.method manual ipv4.addresses 4.4.4.1/30
+
+
+
