9c90e3358928dd61c1019d446be512800c514aac
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-09 13:38:17 +0300

    Add files via upload

--- hq-r.sh
+++ hq-r.sh
@@ -3,6 +3,8 @@ nmcli con modify ens18 ipv6.method manual ipv6.addresses 2024:1::2/64
 nmcli con modify ens18 ipv6.gateway 2024:1::1
 nmcli con modify ens18 ipv4.method manual ipv4.addresses 1.1.1.2/30
 nmcli con modify ens18 ipv4.gateway 1.1.1.1
+nmcli con modify ens18 ipv4.dns 8.8.8.8
+nmcli con modify ens18 ipv6.dns 2001:4860:4860::8888
 
 nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses FD24:172::1/122
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 172.16.100.1/26
@@ -28,8 +30,34 @@ sed -i '/id=ip-tunnel-tun1/d' /etc/NetworkManager/system-connections/ip-tunnel-t
 sed -i '2i\id=tun1' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 
 dnf install -y frr
+sed -i '/ospfd=no/d' /etc/frr/daemons
+sed -i '18i\ospfd=yes' /etc/frr/daemons
+sed -i '/ospf6d=no/d' /etc/frr/daemons
+sed -i '19i\ospf6d=yes' /etc/frr/daemons
+systemctl restart frr
 systemctl enable --now frr
 
+vtysh -c "configure terminal" \
+    -c "router ospf" \
+    -c "passive-interface default" \
+    -c "network 172.16.100.0/26 area 0" \
+    -c "network 10.10.10.0/30 area 0" \
+    -c "exit" \
+    -c "interface tun1" \
+    -c "no ip ospf network broadcast" \
+    -c "no ip ospf passive" \
+    -c "exit" \
+    -c "do write" \
+    -c "router ospf6" \
+    -c "ospf6 router-id 1.1.1.1" \
+    -c "exit" \
+    -c "interface tun1" \
+    -c "ipv6 ospf6 area 0" \
+    -c "exit" \
+    -c "interface ens18" \
+    -c "ipv6 ospf6 area 0" \
+    -c "exit" \
+    -c "do write"
 
 
 
