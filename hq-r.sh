9b7d438d27296f0daa16e1365081756f2dad0320
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-11 08:39:20 +0300

    Add files via upload

--- hq-r.sh
+++ hq-r.sh
@@ -79,8 +79,8 @@ echo "network_admin:P@ssw0rd" | chpasswd
 
 mkdir /var/{backup,backup-script}
 echo -e '#!/bin/bash\n\ndata=$(date +%d.%m.%Y-%H:%M:%S)\nmkdir /var/backup/$data\ncp -r /etc/frr /var/backup/$data\ncp -r /etc/nftables /var/backup/$data\ncp -r /etc/NetworkManager/system-connections /var/backup/$data\ncp -r /etc/dhcp /var/backup/$data\ncd /var/backup\ntar czfv "./$data.tar.gz" ./$data\nrm -r /var/backup/$data' > /var/backup-script/backup.sh
-chmod +x /var/backup/backup.sh
-/var/backup/backup.sh
+chmod +x /var/backup-script/backup.sh
+/var/backup-script/backup.sh
 
 hostnamectl set-hostname hq-r; exec bash
 
