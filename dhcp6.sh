bb90210b99299d7bc6946774469d86d56341114b
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-11 08:39:44 +0300

    Add files via upload

--- dhcp6.sh
+++ dhcp6.sh
@@ -8,7 +8,7 @@ systemctl enable --now dhcpd6
 echo -e "на hq-srv\nзаходим в nmtui ens18\nставим ipv6 автоматически(только dhcp)\nперезагружаем сетевой интерфейс(systemctl restart NetworkManager)\nнажмите любую клавишу для продолжения скрипта <3"
 read -n 1 -s -r -p ""
 
-echo -e "host hq-srv {\n\thost-identifier option\n\t\tdhcp6.client-id 00:04:17:06:02:ee:33:c3:7b:49:af:a0:d9:a5:44:b1:67:f1;\n\tfixed-address6 FD24:172::2;\n\tfixed-prefix6 FD24:172::/172;\n\toption dhcp6.name-servers FD24:172:2;\n}" >> /etc/dhcp/dhcpd6.conf
+echo -e "host hq-srv {\n\thost-identifier option\n\t\tdhcp6.client-id 00:04:17:06:02:ee:33:c3:7b:49:af:a0:d9:a5:44:b1:67:f1;\n\tfixed-address6 FD24:172::2;\n\tfixed-prefix6 FD24:172::/122;\n\toption dhcp6.name-servers FD24:172::2;\n}" >> /etc/dhcp/dhcpd6.conf
 systemctl restart dhcpd6
 
 sed -i '6,$d' /etc/radvd.conf
