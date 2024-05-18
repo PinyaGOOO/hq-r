3996073cff6a0d2d7138ea5e10f1bd72b1d514b5
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-18 12:50:18 +0300

    Add files via upload

--- hq-r.sh
+++ hq-r.sh
@@ -5,33 +5,23 @@ dnf install -y nftables
 dnf install -y frr
 dnf install -y dhcp-server
 
-echo -n "Napishi MAC path:HQ-SRV=HARDWARE-net0: "
-read mac
-
-nmcli con modify ens18 ipv6.method manual ipv6.addresses 2024:1::2/64
-nmcli con modify ens18 ipv6.gateway 2024:1::1
 nmcli con modify ens18 ipv4.method manual ipv4.addresses 1.1.1.2/30
 nmcli con modify ens18 ipv4.gateway 1.1.1.1
 nmcli con modify ens18 ipv4.dns 8.8.8.8
-nmcli con modify ens18 ipv6.dns 2001:4860:4860::8888
 
-nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses FD24:172::1/122
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 172.16.100.1/26
 
-nmcli con modify Проводное\ подключение\ 2 ipv6.method manual ipv6.addresses 2024:4::1/64
-nmcli con modify Проводное\ подключение\ 2 ipv6.gateway 2024:4::2
 nmcli con modify Проводное\ подключение\ 2 ipv4.method manual ipv4.addresses 4.4.4.1/30
 
-echo -e "net.ipv4.ip_forward=1\n\nnet.ipv6.conf.all.forwarding=1\n\nnet.ipv6.conf.ens18.accept_ra=2" >> /etc/sysctl.conf
+echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
-echo -e 'table inet my_nat {\n\tchain prerouting {\n\ttype nat hook prerouting priority filter; policy accept;\n\tip daddr 4.4.4.1 tcp dport 22 dnat ip to 172.16.100.2:2222\n\tip daddr 1.1.1.2 tcp dport 22 dnat ip to 172.16.100.2:2222\n\tip6 daddr 2024:4::1 tcp dport 22 dnat ip6 to [fd24:172::2]:2222\n\tip6 daddr 2024:1::2 tcp dport 22 dnat ip6 to [fd24:172::2]:2222\n\t}\n\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/hq-r.nft
+echo -e 'table inet my_nat {\n\tchain prerouting {\n\ttype nat hook prerouting priority filter; policy accept;\n\tip daddr 4.4.4.1 tcp dport 22 dnat ip to 172.16.100.2:2222\n\tip daddr 1.1.1.2 tcp dport 22 dnat ip to 172.16.100.2:2222\n\t}\n\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/hq-r.nft
 echo 'include "/etc/nftables/hq-r.nft"' >> /etc/sysconfig/nftables.conf
 systemctl enable --now nftables
 
 nmcli con add type ip-tunnel ifname tun1 mode gre remote 2.2.2.2 local 1.1.1.2
 nmcli con modify ip-tunnel-tun1 ipv4.method manual ipv4.addresses 10.10.10.1/30
-nmcli con modify ip-tunnel-tun1 ipv6.method manual ipv6.addresses FD24:10::1/64
 nmcli connection modify ip-tunnel-tun1 ip-tunnel.ttl 64
 sed -i '11i\parent=ens18' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 sed -i '/id=ip-tunnel-tun1/d' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
@@ -54,21 +44,12 @@ vtysh -c "configure terminal" \
     -c "no ip ospf network broadcast" \
     -c "no ip ospf passive" \
     -c "exit" \
-    -c "do write" \
-    -c "router ospf6" \
-    -c "ospf6 router-id 1.1.1.1" \
-    -c "exit" \
-    -c "interface tun1" \
-    -c "ipv6 ospf6 area 0" \
-    -c "exit" \
-    -c "interface ens18" \
-    -c "ipv6 ospf6 area 0" \
-    -c "exit" \
-    -c "do write"
+    -c "do write" 
 
 
 
-echo -e "subnet 172.16.100.0 netmask 255.255.255.192 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\nhost hq-srv {\n\thardware ethernet $mac;\n\tfixed-address 172.16.100.2;\n\toption domain-name-servers 8.8.8.8;\n}" >> /etc/dhcp/dhcpd.conf
+
+echo -e "subnet 172.16.100.0 netmask 255.255.255.192 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\nhost hq-srv {\n\thardware ethernet BC:24:11:FD:81:60;\n\tfixed-address 172.16.100.2;\n\toption domain-name-servers 8.8.8.8;\n}" >> /etc/dhcp/dhcpd.conf
 echo -e "DHCPDARGS=ens19" >>/etc/sysconfig/dhcpd
 systemctl restart dhcpd
 systemctl enable --now dhcpd
