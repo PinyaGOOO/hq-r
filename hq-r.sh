9ab94e47c66461d07917f8a8d2d1e5f20b96820e
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-06-06 20:20:04 +0300

    Add files via upload

--- hq-r.sh
+++ hq-r.sh
@@ -9,14 +9,14 @@ nmcli con modify ens18 ipv4.method manual ipv4.addresses 1.1.1.2/30
 nmcli con modify ens18 ipv4.gateway 1.1.1.1
 nmcli con modify ens18 ipv4.dns 8.8.8.8
 
-nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 172.16.100.1/26
+nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 172.16.100.1/28
 
 nmcli con modify Проводное\ подключение\ 2 ipv4.method manual ipv4.addresses 4.4.4.1/30
 
 echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
-echo -e 'table inet my_nat {\n\tchain prerouting {\n\ttype nat hook prerouting priority filter; policy accept;\n\tip daddr 4.4.4.1 tcp dport 22 dnat ip to 172.16.100.2:2222\n\tip daddr 1.1.1.2 tcp dport 22 dnat ip to 172.16.100.2:2222\n\t}\n\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/hq-r.nft
+echo -e 'table inet my_nat {\n\tchain prerouting {\n\ttype nat hook prerouting priority filter; policy accept;\n\tip daddr 4.4.4.1 tcp dport 22 dnat ip to 172.16.100.2:2020\n\tip daddr 1.1.1.2 tcp dport 22 dnat ip to 172.16.100.2:2020\n\t}\n\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/hq-r.nft
 echo 'include "/etc/nftables/hq-r.nft"' >> /etc/sysconfig/nftables.conf
 systemctl enable --now nftables
 
@@ -37,7 +37,7 @@ systemctl enable --now frr
 vtysh -c "configure terminal" \
     -c "router ospf" \
     -c "passive-interface default" \
-    -c "network 172.16.100.0/26 area 0" \
+    -c "network 172.16.100.0/28 area 0" \
     -c "network 10.10.10.0/30 area 0" \
     -c "network 4.4.4.0/30 area 0"\
     -c "exit" \
@@ -50,7 +50,7 @@ vtysh -c "configure terminal" \
 
 
 
-echo -e "subnet 172.16.100.0 netmask 255.255.255.192 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\nhost hq-srv {\n\thardware ethernet BC:24:11:FD:81:60;\n\tfixed-address 172.16.100.2;\n\toption domain-name-servers 8.8.8.8;\n}" >> /etc/dhcp/dhcpd.conf
+echo -e "subnet 172.16.100.0 netmask 255.255.255.240 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\nhost hq-srv {\n\thardware ethernet BC:24:11:FD:81:60;\n\tfixed-address 172.16.100.2;\n\toption domain-name-servers 8.8.8.8;\n}" >> /etc/dhcp/dhcpd.conf
 echo -e "DHCPDARGS=ens19" >>/etc/sysconfig/dhcpd
 systemctl restart dhcpd
 systemctl enable --now dhcpd
@@ -65,14 +65,13 @@ echo -e '#!/bin/bash\n\ndata=$(date +%d.%m.%Y-%H:%M:%S)\nmkdir /var/backup/$data
 chmod +x /var/backup-script/backup.sh
 /var/backup-script/backup.sh
 
-timedatectl set-timezone Europe/Moscow
 sed -i '3s/^/#/' /etc/chrony.conf
 sed -i '4s/^/#/' /etc/chrony.conf
 sed -i '5s/^/#/' /etc/chrony.conf
 sed -i '6s/^/#/' /etc/chrony.conf
 sed -i '7a\server 127.0.0.1 iburst prefer' /etc/chrony.conf
 sed -i '8a\hwtimestamp *' /etc/chrony.conf
-sed -i '9a\local stratum 5' /etc/chrony.conf
+sed -i '9a\local stratum 6' /etc/chrony.conf
 sed -i '10a\allow 0/0' /etc/chrony.conf
 sed -i '11a\allow ::/0' /etc/chrony.conf
 
